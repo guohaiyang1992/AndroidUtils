7f546380d583f2975adbce220830fa2788a9433c
simon <guohaiyang1992@126.com>
2017-05-16 13:49:44 +0800

    增加文件和字符串处理工具类

---
+++ FileUtils.java
@@ -0,0 +1,76 @@
+package com.guohaiyang.bitmaprecycledemo.common;
+
+import java.io.File;
+
+/**
+ * Created by guohaiyang on 2017/5/16.
+ * 该工具类可以和StringUtils 结合可以判断地址是否符合标准
+ */
+
+public class FileUtils {
+
+    //--检查类型方法--------------------
+
+    /**
+     * 检查文件是否存在
+     *
+     * @param path --文件的地址
+     * @return true标识存在，false标识不存在
+     */
+    public static boolean checkFileIsExist(String path) {
+        return checkFileIsExist(pathToFile(path));
+    }
+
+
+    /**
+     * 检查文件是否存在
+     *
+     * @param file --文件
+     * @return true标识存在，false标识不存在
+     */
+    public static boolean checkFileIsExist(File file) {
+        if (file != null) {
+            return file.exists();
+        }
+        return false;
+    }
+
+
+    //--操作类型方法----------------------
+
+    /**
+     * 如果存在旧文件则删除，如果其文件的所在的文件夹不在，则创建-用于清除旧数据，写入新数据
+     *
+     * @param path --文件地址
+     */
+    public static void deleteOldFileAndCreateFolder(String path) {
+        deleteOldFileAndCreateFolder(pathToFile(path));
+    }
+
+    /**
+     * * 如果存在旧文件则删除，如果其文件的所在的文件夹不在，则创建-用于清除旧数据，写入新数据
+     *
+     * @param file --文件
+     */
+    public static void deleteOldFileAndCreateFolder(File file) {
+
+        if (checkFileIsExist(file)) { //如果存在删除旧的文件
+            file.delete();
+        } else { //不存在则看看是否存在其父路径，不存在则创建
+            File parentFile = file.getParentFile();
+            if (!checkFileIsExist(parentFile)) {
+                parentFile.mkdirs();
+            }
+        }
+    }
+
+    /**
+     * 地址转文件
+     *
+     * @param path --文件地址
+     * @return
+     */
+    public static File pathToFile(String path) {
+        return new File(path);
+    }
+}
---
+++ StringUtils.java
@@ -0,0 +1,104 @@
+package com.guohaiyang.bitmaprecycledemo.common;
+
+import android.text.TextUtils;
+
+/**
+ * Created by guohaiyang on 2017/5/16.
+ */
+
+public class StringUtils {
+    //--默认值--
+    private static final int DEFAULT_INT = Integer.MIN_VALUE;
+    private static final float DEFAULT_FLOAT = Float.MIN_VALUE;
+    private static final double DEFAULT_DOUBLE = Double.MIN_VALUE;
+
+    //--检查类型方法--
+
+    /**
+     * 判断是否内容不为空不为null
+     *
+     * @param s --内容
+     * @return
+     */
+    public static boolean checkValueIsUsefull(String s) {
+        return !TextUtils.isEmpty(s);
+    }
+
+    //--转换类型方法--
+
+    /**
+     * 安全的字符串转int
+     *
+     * @param s --内容
+     * @return
+     */
+    public static int safeStringToInt(String s) {
+        return safeStringToInt(s, DEFAULT_INT);
+    }
+
+    /**
+     * 安全的转字符串转int
+     *
+     * @param s         --内容
+     * @param defaulInt --默认值
+     * @return
+     */
+    public static int safeStringToInt(String s, int defaulInt) {
+        try {
+            return Integer.parseInt(s);
+        } catch (Exception e) {
+            return defaulInt;
+        }
+    }
+
+
+    /**
+     * 安全的字符串转float
+     *
+     * @param s --内容
+     * @return
+     */
+    public static float safeStringToFloat(String s) {
+        return safeStringToFloat(s, DEFAULT_FLOAT);
+    }
+
+    /**
+     * 安全的转字符串转float
+     *
+     * @param s            --内容
+     * @param defaultFloat --默认值
+     * @return
+     */
+    public static float safeStringToFloat(String s, float defaultFloat) {
+        try {
+            return Integer.parseInt(s);
+        } catch (Exception e) {
+            return defaultFloat;
+        }
+    }
+
+    /**
+     * 安全的字符串转double
+     *
+     * @param s --内容
+     * @return
+     */
+    public static double safeStringToDouble(String s) {
+        return safeStringToDouble(s, DEFAULT_DOUBLE);
+    }
+
+    /**
+     * 安全的转字符串转double
+     *
+     * @param s             --内容
+     * @param defaultDouble --默认值
+     * @return
+     */
+    public static double safeStringToDouble(String s, double defaultDouble) {
+        try {
+            return Integer.parseInt(s);
+        } catch (Exception e) {
+            return defaultDouble;
+        }
+    }
+}
