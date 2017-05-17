50f2bdab1dcba0c419c7da8bc929e59056484f62
Simon <guohaiyang1992@126.com>
2017-05-17 16:54:36 +0800

    增加部分判断方法

--- ThreadUtils.java
+++ ThreadUtils.java
@@ -1,4 +1,4 @@
-package com.hfga.resumebooklib.utils.thread;
+package com.guohaiyang.hookactivity.ThreadHelper;
 
 import android.os.Handler;
 import android.os.Looper;
@@ -7,6 +7,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class ThreadUtils {
+    //--运行相关--
     private static Handler handler = null;
 
     static {
@@ -39,6 +40,34 @@ public class ThreadUtils {
         if (mCacheExcutor != null) {
             mCacheExcutor.shutdown();
         }
+    }
+    //--判断相关--
+
+    /**
+     * 判断是不是主线程
+     *
+     * @return
+     */
+    public static boolean isMainThread() {
+        return Looper.getMainLooper() == Looper.myLooper();
+    }
+
+    /**
+     * 判断是不是后台线程
+     *
+     * @return
+     */
+    public static boolean isBackThread() {
+        return !isMainThread();
+    }
+
+    /**
+     * 检查当前运行的线程是否在活动状态
+     *
+     * @return
+     */
+    public static boolean checkIsAlive() {
+        return Thread.currentThread().isAlive();
     }
 
 
